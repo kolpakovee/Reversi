87dc42371027d8215fa59db4b349b6f306d309b9
kolpakovee <eekolpakov@edu.hse.ru>
2022-12-03 17:57:15 +0300

    edit class Game

--- src/kolpakovee/Reversy/Model/Game.java
+++ src/kolpakovee/Reversy/Model/Game.java
@@ -2,8 +2,9 @@ package kolpakovee.Reversy.Model;
 
 
 import kolpakovee.Reversy.ConsoleIO;
-import kolpakovee.Reversy.PlayerTurn;
+import kolpakovee.Reversy.View.Drawing;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
@@ -24,14 +25,14 @@ public class Game {
                         player2.cellsOnTheField.size();
                 // сделали ходы
                 if (gameMode == GameMode.easy) {
-                    PlayerTurn.playerMove(field, player1, player2);
-                    PlayerTurn.easyComputerMove(field, player1, player2);
+                    playerMove(player1, player2);
+                    easyComputerMove(player1, player2);
                 } else if (gameMode == GameMode.pvp) {
-                    PlayerTurn.playerMove(field, player1, player2);
-                    PlayerTurn.playerMove(field, player2, player1);
+                    playerMove(player1, player2);
+                    playerMove(player2, player1);
                 } else if (gameMode == GameMode.advanced) {
-                    PlayerTurn.playerMove(field, player1, player2);
-                    PlayerTurn.advancedComputerMove(field, player1, player2);
+                    playerMove(player1, player2);
+                    advancedComputerMove(field, player1, player2);
                 }
             }
             // если ходить некуда или кол-во шашек не изменилось -> break
@@ -48,6 +49,164 @@ public class Game {
     }
 
     public Game() {
+    }
+
+    public ArrayList<Cell> possibleMoves(ArrayList<Cell> freeCells, Player player) {
+        ArrayList<Cell> cells = new ArrayList<>();
+        for (Cell cell : freeCells) {
+            if (getFunctionResult(cell, player.getColor()) >= 1) {
+                cells.add(cell);
+            }
+        }
+        return cells;
+    }
+
+    public double getFunctionResult(Cell cell, Color color) {
+        double result = 0;
+        if (Cell.isCornerCell(cell.getX(), cell.getY())) {
+            result += 0.8;
+        } else if (Cell.isEdgeCell(cell.getX(), cell.getY())) {
+            result += 0.4;
+        }
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                result += getFunctionResultHelper(cell, color, i, j);
+            }
+        }
+        return result;
+    }
+
+    public double getFunctionResultHelper(Cell cell, Color color, int forX, int forY) {
+        double result = 0;
+        int x = cell.getX() + forX;
+        int y = cell.getY() + forY;
+        boolean canReturn = false;
+        while (x >= 0 && y >= 0 && x <= 7 && y <= 7) {
+            // если цвет врага
+            if (field.cells[x][y].getColor() != color && field.cells[x][y].getColor() != Color.gray) {
+                x += forX;
+                y += forY;
+                if (Cell.isEdgeCell(x, y)) {
+                    result += 2;
+                } else {
+                    result += 1;
+                }
+            }
+            // если наш цвет
+            else if (field.cells[x][y].getColor() == color) {
+                canReturn = true;
+                break;
+            }
+            // если серый
+            else {
+                break;
+            }
+        }
+        if (canReturn) {
+            return result;
+        }
+        return 0;
+    }
+
+    /**
+     * Функция поиска возможных ходов
+     * @param cells клетки врага
+     * @return массив клеток - возможных ходов
+     */
+    // в класс ход
+    public ArrayList<Cell> findFreeCells(ArrayList<Cell> cells) {
+        ArrayList<Cell> resultList = new ArrayList<>();
+        for (Cell cell : cells) {
+            for (int j = cell.getX() - 1; j <= cell.getX() + 1; j++) {
+                for (int k = cell.getY() - 1; k <= cell.getY() + 1; k++) {
+                    if (j >= 0 && j <= 7 &&
+                            k >= 0 && k <= 7 &&
+                            field.cells[j][k].getColor() == Color.gray) {
+                        resultList.add(field.cells[j][k]);
+                    }
+                }
+            }
+        }
+        return resultList;
+    }
+
+    public void playerMove(Player player, Player enemy) {
+        ArrayList<Cell> freeCells = findFreeCells(enemy.cellsOnTheField);
+        // Находим возможные ходы для player1
+        ArrayList<Cell> possibleMoves = possibleMoves(freeCells, player);
+        if (possibleMoves.size() == 0) {
+            return;
+        }
+        // Показываем игроку, куда он может сходить
+        Drawing.repaintPossibleMoves(possibleMoves, field, player, enemy);
+        // Метод получения координаты от пользователя
+        Cell cell = ConsoleIO.getCoordinatesFromConsole(field, player, possibleMoves);
+        // Делаем ход за пользователя
+        cell.setColor(player.getColor());
+        Drawing.repaint(cell, player, enemy, field);
+    }
+
+    /**
+     * Метод для поиска лучшего хода режима Easy
+     */
+    public void easyComputerMove(Player player, Player computer) {
+        double bestRes = 0;
+        ArrayList<Cell> freeCells = findFreeCells(player.cellsOnTheField);
+        Cell c = new Cell(0, 0);
+        boolean canMove = false;
+        for (Cell cell : freeCells) {
+            double res = getFunctionResult(cell, Color.purple);
+            if (res > bestRes && res >= 1) {
+                canMove = true;
+                bestRes = res;
+                c = cell;
+            }
+        }
+        if (canMove) {
+            c.setColor(Color.purple);
+            Drawing.repaint(c, player, computer, field);
+            ConsoleIO.moveToConsole(c);
+        }
+    }
+
+    public void advancedComputerMove(Field field, Player player, Player computer) {
+        // делаю -100, потому что могут быть ситуации, когда все ходы res могут быть < 0
+        double bestComputerRes = -100;
+        ArrayList<Cell> freeCells = findFreeCells(player.cellsOnTheField);
+        Cell c = new Cell(0, 0);
+        boolean canMove = false;
+        // проходимся по всем ходам компьютера
+        for (Cell cell : freeCells) {
+            double res = getFunctionResult(cell, Color.purple);
+            // проверяем, можем ли так ходить
+            if (res >= 1) {
+                // если можем, то временно перекрашиваем ячейку
+                cell.setColor(Color.purple);
+                double bestPlayerRes = 0;
+                // находим все ходы для пользователя
+                ArrayList<Cell> freeCellsForPlayer = findFreeCells(computer.cellsOnTheField);
+                // проходимся по возможным ходам игрока
+                for (Cell playerCell : freeCellsForPlayer) {
+                    // получаем значение функции для хода пользователя
+                    double playerRes = getFunctionResult(playerCell, Color.white);
+                    if (playerRes > bestPlayerRes) {
+                        bestPlayerRes = playerRes;
+                    }
+                }
+                // уже есть лучший ход пользователя bestPlayerRes проверяем, подходит ли нам
+                if (res - bestPlayerRes > bestComputerRes) {
+                    bestComputerRes = res - bestPlayerRes;
+                    canMove = true;
+                    c = cell;
+                }
+                cell.setColor(Color.gray);
+            }
+        }
+        if (canMove) {
+            c.setColor(Color.purple);
+            Drawing.repaint(c, player, computer, field);
+            ConsoleIO.moveToConsole(c);
+        }
     }
 
     public boolean setGameMode() {
